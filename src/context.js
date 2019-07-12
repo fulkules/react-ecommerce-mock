2e8703c2d0f558d32127f5a22779065f3224f610
Barry Fulk <bfulk@Barrys-MacBook-Pro.local>
2019-07-12 14:28:43 -0600

    remove item working

--- src/context.js
+++ src/context.js
@@ -82,33 +82,54 @@ class ProductProvider extends Component {
   };
 
   removeItem = id => {
-    console.log("remove item method");
+    let tempProducts = [...this.state.products];
+    let tempCart = [...this.state.cart];
+
+    tempCart = tempCart.filter(item => item.id !== id);
+    const index = tempProducts.indexOf(this.getItem(id));
+    let removedProduct = tempProducts[index];
+    removedProduct.inCart = false;
+    removedProduct.count = 0;
+    removedProduct.total = 0;
+    this.setState(
+      () => {
+        return {
+          cart: [...tempCart],
+          products: [...tempProducts]
+        };
+      },
+      () => {
+        this.addTotals();
+      }
+    );
   };
 
   clearCart = () => {
-    this.setState(() => {
-      return { cart: [] }
-    }, () => {
-      this.setProducts();
-      this.addTotals();
-    })
+    this.setState(
+      () => {
+        return { cart: [] };
+      },
+      () => {
+        this.setProducts();
+        this.addTotals();
+      }
+    );
   };
 
   addTotals = () => {
     let subTotal = 0;
-    this.state.cart.map(item => (subTotal += item.total))
-    const tempTax = subTotal * 0.07;
-    const tax = parseFloat(tempTax.toFixed(2))
-    const total = subTotal + tax
+    this.state.cart.map(item => (subTotal += item.total));
+    const tempTax = subTotal * 0.08;
+    const tax = parseFloat(tempTax.toFixed(2));
+    const total = subTotal + tax;
     this.setState(() => {
       return {
         cartSubTotal: subTotal,
         cartTax: tax,
         cartTotal: total
-
-      }
-    })
-  }
+      };
+    });
+  };
 
   render() {
     return (
