17f7209d85f9cb65437c7409c738b9f88a1fde22
Barry Fulk <bfulk@Barrys-MacBook-Pro.local>
2019-07-12 14:58:26 -0600

    decrement done

--- src/context.js
+++ src/context.js
@@ -74,11 +74,47 @@ class ProductProvider extends Component {
   };
 
   increment = id => {
-    console.log("increment method");
+    let tempCart = [...this.state.cart];
+    const selectedProduct = tempCart.find(item => item.id === id);
+
+    const index = tempCart.indexOf(selectedProduct);
+    const product = tempCart[index];
+
+    product.count = product.count + 1;
+    product.total = product.count * product.price;
+
+    this.setState(
+      () => {
+        return { cart: [...tempCart] };
+      },
+      () => {
+        this.addTotals();
+      }
+    );
   };
 
   decrement = id => {
-    console.log("decrement method");
+    let tempCart = [...this.state.cart];
+    const selectedProduct = tempCart.find(item => item.id === id);
+
+    const index = tempCart.indexOf(selectedProduct);
+    const product = tempCart[index];
+
+    product.count = product.count - 1;
+
+    if (product.count === 0) {
+      this.removeItem(id);
+    } else {
+      product.total = product.count * product.price;
+      this.setState(
+        () => {
+          return { cart: [...tempCart] };
+        },
+        () => {
+          this.addTotals();
+        }
+      );
+    }
   };
 
   removeItem = id => {
@@ -121,7 +157,7 @@ class ProductProvider extends Component {
     this.state.cart.map(item => (subTotal += item.total));
     const tempTax = subTotal * 0.08;
     const tax = parseFloat(tempTax.toFixed(2));
-    const total = subTotal + tax;
+    const total = parseFloat(subTotal + tax).toFixed(2);
     this.setState(() => {
       return {
         cartSubTotal: subTotal,
